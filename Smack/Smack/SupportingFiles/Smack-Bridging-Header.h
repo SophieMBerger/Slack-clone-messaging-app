90cf8f65831a8aadca91fd0fb052ce2fa17086fa
SophieMBerger <berger.m.sophie@gmail.com>
2018-08-13 14:28:28 +0200

    Added CocoaPods and SWReveal files

---
+++ Smack/Smack/SupportingFiles/Smack-Bridging-Header.h
@@ -0,0 +1,5 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "SWRevealViewController.h" //access to methods/functions that are in Objective-C files
