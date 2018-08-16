8b4d098334e6aeb493908a920866b3806c9757af
SophieMBerger <berger.m.sophie@gmail.com>
2018-08-16 09:44:28 +0200

    user registered

--- Smack/Smack/Controller/CreateAccountVC.swift
+++ Smack/Smack/Controller/CreateAccountVC.swift
@@ -9,11 +9,39 @@
 import UIKit
 
 class CreateAccountVC: UIViewController {
-
+    
+    
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passTxt: UITextField!
+    @IBOutlet weak var userImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+    
+    
+    @IBAction func createAccountPressed(_ sender: Any) {
+        //To register a user only email/password is needed
+        //guard let = another way of force unwrapping optionals
+        //text field values are optional by default
+        guard let email = emailTxt.text , emailTxt.text != "" else { return}
+        guard let pass = passTxt.text , passTxt.text != "" else { return}
+        
+        AuthService.instance.registerUser(email: email, password: pass) { (success) in
+            if success {
+                print("Registered user!")
+            }
+        }
+    }
+    
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+    }
+    
+    @IBAction func pickBGColorPressed(_ sender: Any) {
+    }
+    
     
     @IBAction func closePressed(_ sender: Any) {
         performSegue(withIdentifier: UNWIND, sender: nil)
