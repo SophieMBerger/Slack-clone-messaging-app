d1a6be63ac67612f80a79db1b10815969b24b4eb
SophieMBerger <berger.m.sophie@gmail.com>
2018-12-19 10:56:47 +0100

    channels added

--- Smack/Smack/Utilities/Constants.swift
+++ Smack/Smack/Utilities/Constants.swift
@@ -24,6 +24,8 @@ let URL_USER_ADD = "\(BASE_URL)user/add"
 
 let URL_USER_BY_EMAIL = "\(BASE_URL)user/byEmail/"
 
+let URL_GET_CHANNELS = "\(BASE_URL)channel/"
+
 //Colors
 let SMACKPURPLEPLACEHOLDER = #colorLiteral(red: 0.3254901961, green: 0.4215201139, blue: 0.7752227187, alpha: 0.5)
 
