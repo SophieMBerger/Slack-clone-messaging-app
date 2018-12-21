9f4aca8a720e37614387ba448570589d1290a2f2
SophieMBerger <berger.m.sophie@gmail.com>
2018-12-21 10:35:52 +0100

    Ability fetch messages

--- Smack/Smack/Services/MessageService.swift
+++ Smack/Smack/Services/MessageService.swift
@@ -17,6 +17,8 @@ class MessageService {
     
     //    an array of objects of Channel modle
     var channels = [Channel]()
+    //    array of messages
+    var messages = [Message]()
     //    the currently selected channel (optional)
     var selectedChannel: Channel?
     
@@ -53,6 +55,38 @@ class MessageService {
                 debugPrint(response.result.error as Any)
             }
         }
+    }
+    
+    func findAllMessagesForChannel(channelId: String, completion: @escaping CompletionHandler) {
+        Alamofire.request("\(URL_GET_MESSAGES)\(channelId)", method: .get, parameters: nil, encoding: JSONEncoding.default, headers: BEARER_HEADER).responseJSON { (response) in
+            
+            if response.result.error == nil {
+                self.clearMessages()
+                guard let data = response.data else {return}
+                let json = try! JSON(data: data).array
+                for item in json! {
+                    let messageBody = item["messageBody"].stringValue
+                    let channelId = item["channelId"].stringValue
+                    let id = item["_id"].stringValue
+                    let userName = item["userName"].stringValue
+                    let userAvatar = item["userAvatar"].stringValue
+                    let userAvatarColor = item["userAvatarColor"].stringValue
+                    let timeStamp = item["timeStamp"].stringValue
+                    
+                    let message = Message(message: messageBody, userName: userName, channelId: channelId, userAvatar: userAvatar, userAvatarColor: userAvatarColor, id: id, timeStamp: timeStamp)
+                    
+                    self.messages.append(message)
+                }
+                completion(true)
+            } else {
+                debugPrint(response.result.error as Any)
+                completion(false)
+            }
+        }
+    }
+    
+    func clearMessages() {
+        messages.removeAll()
     }
     
     func clearChannels() {
